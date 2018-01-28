5fd1326b739d9d8ee89d3a264052b70c5c7aa2ba
triestpa <patrick.triest@gmail.com>
2018-01-27 19:57:21 -0500

    Rename base to utils

--- install/utils.sh
+++ install/utils.sh
@@ -2,4 +2,4 @@
 apt-get install -y apt-utils
 apt-get install -y software-properties-common python-software-properties
 apt-get -y upgrade
-apt-get install -y curl wget git
+apt-get install -y curl wget git tmux
