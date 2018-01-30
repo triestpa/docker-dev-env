e9810a35e6632cc60a71e8908e88f0dc7e5e0781
triestpa <patrick.triest@gmail.com>
2018-01-29 23:54:58 -0500

    Switch default node version to 8

--- install/node.sh
+++ install/node.sh
@@ -3,7 +3,7 @@ wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
-nvm install node
-nvm use node
+nvm install 8
+nvm use 8
 apt-get install -y build-essential libssl-dev
 npm install -g create-react-app create-react-native-app
