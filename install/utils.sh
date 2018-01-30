f1632f570d3c1521c364d7fff6148a6bebfeeda6
triestpa <patrick.triest@gmail.com>
2018-01-29 23:56:35 -0500

    Add locale config to image

--- install/utils.sh
+++ install/utils.sh
@@ -2,4 +2,5 @@
 apt-get install -y apt-utils
 apt-get install -y software-properties-common python-software-properties
 apt-get -y upgrade
-apt-get install -y curl wget git tmux
+apt-get install -y curl wget git tmux locales
+locale-gen "en_US.UTF-8"
