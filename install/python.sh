b6cbf9f678ee4877840ddd0064f479722d2ef016
triestpa <patrick.triest@gmail.com>
2018-01-29 23:56:02 -0500

    Remove scipy from python install script

--- install/python.sh
+++ install/python.sh
@@ -3,8 +3,5 @@ apt-get install -y build-essential libssl-dev libffi-dev python-dev \
     gfortran libatlas-base-dev  libbz2-dev libpng-dev libfreetype6-dev \
     libreadline-dev libsqlite3-dev tk-dev
 
-apt-get install -y python3-pip
+apt-get install -y python-dev python-pip python3-dev python3-pip
 pip3 install --upgrade pip
-pip3 install numpy scipy matplotlib ipython jupyter pandas sympy nose
-pip3 install scikit-learn
-pip3 install tensorflow keras
