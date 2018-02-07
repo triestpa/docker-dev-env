a03d870c973d09309a3069e048b3352cf519fb0e
triestpa <patrick.triest@gmail.com>
2018-02-06 21:59:25 -0500

    Clear apt cache during setup

--- install/utils.sh
+++ install/utils.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 apt-get install -y apt-utils
 apt-get install -y software-properties-common python-software-properties
+apt-get -y clean
 apt-get -y upgrade
-apt-get install -y curl wget git tmux locales
+apt-get install -y curl wget
+apt-get install -y git
+apt-get install -y tmux
+apt-get install -y locales
 locale-gen "en_US.UTF-8"
